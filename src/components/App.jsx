d8ea04cf169ba6ebc5992cf1d77af60f5772dcc1
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-05-01 17:03:56 +0300

    add new and hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { nanoid } from 'nanoid';
+import { Section, Title } from './App.styled';
+
+import Form from './Form';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+import { useLocalStorage } from 'hooks/useLocalStoage';
+
+export function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addNewContact = data => {
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+    contacts.some(({ name }) => name === data.name)
+      ? Notify.warning(`${data.name} is already in contacts`)
+      : setContacts(prev => [...prev, newContact]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+ return (
+   <>
+     <Section>
+       <Title>Phonebook</Title>
+       <Form addNewContact={addNewContact} />
+     </Section>
+     <Section>
+       <Title>Contacts</Title>
+       <Filter value={filter} onChange={changeFilter} />
+       <ContactsList
+         contacts={getVisibleContacts()}
+         deleteContact={deleteContact}
+       />
+     </Section>
+   </>
+ );
+}
---
+++ src/components/App.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  color: #160b0f;
+  font-size: 35px;
+`;
+export const Section = styled.section`
+  width: 500px;
+  margin: 35px;
+`;
